7eb8b869d5866ffe1e5348a4050ebce4d86e8994
Mx444 <mybbtester@gmail.com>
2025-02-19 17:38:19 +0100

    feat: array

---
+++ src/Array.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] numeri = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        String num = Arrays.toString(numeri);
+        System.out.println("Meotdo toString : " + num + "\n");
+        System.out.println("Meotdo For : ");
+
+        for (int i = 0; i < numeri.length; i++) {
+            System.out.println(numeri[i]);
+        }
+    }
+}
