1fb2ebb3ee6a7ffd18654c8f42ee5920374967b0
Mx444 <mybbtester@gmail.com>
2025-02-20 09:25:16 +0000

    feat: modulo

---
+++ src/PariDispari.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PariDispari {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try (scanner) {
+            System.out.println("Inserisci un numero : ");
+            int numero = scanner.nextInt();
+
+            if (numero % 2 == 0)
+                System.out.println("Numero Pari :" + numero);
+            else
+                System.out.println("Numero Dispari :" + numero);
+
+        }
+
+    }
+
+}
